88b989723a2842228410c775e2999a14fa61cd8e
Jesse Weaver <pianohacker@gmail.com>
2013-03-19 01:50:32 -0600

    Implemented proper align-right for text widgets

--- src/widgets.c
+++ src/widgets.c
@@ -82,6 +82,10 @@ static void _canvas_resize_cb(ClutterActor *actor, GParamSpec *spec, ClutterCanv
 	clutter_canvas_set_size(canvas, clutter_actor_get_width(actor), clutter_actor_get_height(actor));
 }
 
+static void _align_right_resize_cb(ClutterActor *actor, GParamSpec *spec, gpointer user_data) {
+	clutter_actor_set_translation(actor, -clutter_actor_get_width(actor), 0, 0);
+}
+
 static ClutterActor* _canvas_widget_new(GCallback draw_cb, NubeWidgetConfig *widget_config) {
 	ClutterActor *widget = clutter_actor_new();
 
@@ -89,14 +93,6 @@ static ClutterActor* _canvas_widget_new(GCallback draw_cb, NubeWidgetConfig *wid
 	g_signal_connect(content, "draw", draw_cb, (NubeWidgetConfig*) widget_config);
 	g_signal_connect(widget, "notify::allocation", G_CALLBACK(_canvas_resize_cb), content);
 	clutter_actor_set_content(widget, content);
-
-	return widget;
-}
-
-ClutterActor* _clock_init(NubeWidgetConfig *widget_config) {
-	ClutterActor *widget = clutter_text_new();
-	clutter_text_set_single_line_mode(CLUTTER_TEXT(widget), TRUE);
-	clutter_text_set_color(CLUTTER_TEXT(widget), nube_config.fg);
 
 	return widget;
 }
@@ -114,6 +110,12 @@ ClutterActor* _text_init(NubeWidgetConfig *widget_config) {
 	ClutterActor *widget = clutter_text_new();
 	clutter_text_set_single_line_mode(CLUTTER_TEXT(widget), TRUE);
 	clutter_text_set_color(CLUTTER_TEXT(widget), nube_config.fg);
+
+	GValue *value;
+	if ((value = g_datalist_get_data(&widget_config->props, "align")) && strcmp(g_value_get_string(value), "right") == 0) {
+		// Hack because anchor-from-gravity is deprecated for some retarded reason
+		g_signal_connect(widget, "notify::allocation", G_CALLBACK(_align_right_resize_cb), NULL);
+	}
 
 	return widget;
 }
@@ -224,12 +226,12 @@ void nube_widget_add(ClutterActor *panel, NubeWidgetConfig *widget_config) {
 
 	g_datalist_foreach((GData**) &widget_config->props, (GDataForeachFunc) _widget_add_property, widget);
 
-	if (widget_config->position) {
-		clutter_actor_set_position(widget, widget_config->position->x + nube_config.glow_size, widget_config->position->y + nube_config.glow_size);
-	}
-
 	if (widget_config->pivot_point) {
 		clutter_actor_set_pivot_point(widget, widget_config->pivot_point->x, widget_config->pivot_point->y);
+	}
+
+	if (widget_config->position) {
+		clutter_actor_set_position(widget, widget_config->position->x + nube_config.glow_size, widget_config->position->y + nube_config.glow_size);
 	}
 
 	g_object_set_data(G_OBJECT(widget), "widget_type", widget_type);
@@ -252,7 +254,7 @@ void nube_widget_type_register(
 }
 
 void nube_widget_types_init() {
-	nube_widget_type_register("clock", _clock_init, _clock_draw);
+	nube_widget_type_register("clock", _text_init, _clock_draw);
 	nube_widget_type_register("icon", _icon_init, NULL);
 	nube_widget_type_register("text", _text_init, _text_draw);
 	nube_widget_type_register("vertical_bar", _vertical_bar_init, _vertical_bar_draw);
